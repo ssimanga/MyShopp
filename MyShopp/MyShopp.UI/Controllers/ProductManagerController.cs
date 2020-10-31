117d4b72aa35f7c8a62cad056b6e1a1b40a5825a
Simanga Shabalala <simangae@gmail.com>
2020-10-31 12:38:13 +0200

    Added Product Image  Upload

--- MyShopp/MyShopp.UI/Controllers/ProductManagerController.cs
+++ MyShopp/MyShopp.UI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,7 +37,7 @@ namespace MyShopp.UI.Controllers
         }
         
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -44,6 +45,11 @@ namespace MyShopp.UI.Controllers
             }
             else
             {
+                if(file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//" + product.Image));
+                }
                 context.Insert(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -66,7 +72,7 @@ namespace MyShopp.UI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (product == null)
@@ -75,13 +81,18 @@ namespace MyShopp.UI.Controllers
             }
             else
             {
+
                 if (!ModelState.IsValid)
                 {
                     return View(product);
                 }
+                if(file != null)
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//" + productToEdit.Image));
+                }
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
                 product.Name = product.Name = product.Name;
                 productToEdit.Price = product.Price;
                 context.Commit();
