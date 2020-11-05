4c44848748c91229e42be6f1a24c8ea9ab37c020
Simanga Shabalala <simangae@gmail.com>
2020-11-05 08:25:43 +0200

    Added Basket Model

--- MyShopp/MyShopp.DataAccess.SQL/DataContext.cs
+++ MyShopp/MyShopp.DataAccess.SQL/DataContext.cs
@@ -18,5 +18,7 @@ namespace MyShopp.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
