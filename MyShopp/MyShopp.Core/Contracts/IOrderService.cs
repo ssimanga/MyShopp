1f4bbb3a958e7df8ed03cd766e09a64360e84b13
Simanga Shabalala <simangae@gmail.com>
2020-11-14 08:26:20 +0200

    Finished the OrderManagement

--- MyShopp/MyShopp.Core/Contracts/IOrderService.cs
+++ MyShopp/MyShopp.Core/Contracts/IOrderService.cs
@@ -11,5 +11,8 @@ namespace MyShopp.Core.Contracts
     public interface IOrderService
     {
         void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
     }
 }
---
+++ MyShopp/MyShopp.UI/Controllers/OrderManagerController.cs
@@ -0,0 +1,54 @@
+﻿using MyShopp.Core.Contracts;
+using MyShopp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShopp.UI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+        public OrderManagerController(IOrderService OrderService)
+        {
+            this.orderService = OrderService;
+        }
+        // GET: OrderManager
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList();
+            return View(orders);
+        }
+
+        public ActionResult UpdateOrder(string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+            ViewBag.StatusList = new List<string>()
+            {
+                "Order Created",
+                "Payment Processed",
+                "Order Shipped",
+                "Order Completed"
+            };
+            return View(order);
+        }
+        [HttpPost]
+        public ActionResult UpdateOrder(Order updatedOrder, string Id)
+        {
+            ViewBag.StatusList = new List<string>()
+            {
+                "Order Created",
+                "Payment Processed",
+                "Order Shipped",
+                "Order Completed"
+            };
+            Order order = orderService.GetOrder(Id);
+            order.OrderStatus = updatedOrder.OrderStatus;
+            orderService.UpdateOrder(order);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
